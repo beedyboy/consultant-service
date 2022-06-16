54bbd8c1cf79fc9ace348eeb0d31082ae4ed72b3
Bolade Akinniyi <boladebode@gmail.com>
2022-06-16 20:50:26 +0100

    updated

--- app/server.js
+++ app/server.js
@@ -36,7 +36,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(routes);
+app.use('https://consultantation-service.herokuapp.com/consultantationmgt',routes);
 app.get("/", (req, res) => {
   res.send("<h2>Hi there!</h2>");
 });
